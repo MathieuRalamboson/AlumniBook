a6b2d9a7ca86a05636433e891c8363e9f019001b
MathieuRalamboson <ralamboson@et.esiea.fr>
2021-04-18 16:32:19 +0200

    Création des WS CRUD coté Front et interface Alumni

---
+++ front/alumniBook-app/src/app/module/alumni/alumni.service.ts
@@ -0,0 +1,62 @@
+import { Injectable } from '@angular/core';
+import { HttpHeaders, HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+import { Alumni } from './alumni';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AlumniService {
+
+  private _server = environment.webservice.server;
+
+  constructor(private http: HttpClient) { 
+
+  }
+
+  /**
+   *  WS pour récuperer tous les alumnis
+   * @returns 
+   */
+  public getAllAlumni(): Observable<any> {
+    const url = `${this._server}/alumni/getAllAlumni`;
+
+    return this.http.get<Alumni[]>(url);
+  }
+
+  /**
+   *  WS pour la création d'un alumni
+   * @param params de l'alumni à ajouter
+   * @returns 
+   */
+  public createAlummni(params: any): Observable<any> {
+    const url = `${this._server}/alumni/create`;
+    return this.http.post<any>(url, params);
+  }
+
+  /**
+   * WS pour la suppression d'un alumni
+   * @param id de l'alumni a supprimer
+   * @returns 
+   */
+  public deleteAlumni(id :number): Observable<any> {
+    const url = `${this._server}/alumni/delete/${id}`;
+    return this.http.delete<any>(url);
+  }
+
+  /**
+   *  WS pour la modification d'un alumni
+   * @param id de l'alummni à modifier
+   * @param params 
+   * @returns 
+   */
+  public updateAlumni(id: number,params :any) : Observable<any> {
+    const url = `${this._server}/alumni/update/${id}`
+    return this.http.put<any>(url,params);
+  }
+
+
+
+
+}
---
+++ front/alumniBook-app/src/app/module/alumni/alumni.ts
@@ -0,0 +1,10 @@
+export interface Alumni {
+    id: number;
+    name: string;
+    code: string;
+    jobTitle: string;
+    email: string;
+    graduationDate: Date;
+    imageUrl: string;
+
+}
